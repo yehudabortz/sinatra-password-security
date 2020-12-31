f132295055c37d58f87ecf4c151bc5ee67d4a026
Yehuda Bortz <yehudabortz@icloud.com>
2020-12-30 22:38:43 -0600

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,14 +17,27 @@ class ApplicationController < Sinatra::Base
 	end
 
 	post "/signup" do
-		#your code here!
+		user = User.new(:username => params[:username], :password => [:password])
+
+		if user.save
+			redirect "/login"
+		else
+			redirect "/failure"
+		end
 	end
 
 	get "/login" do
+
 		erb :login
 	end
 
 	post "/login" do
+		user = User.find_by(:username => params[:username])
+		if user
+			redirect "/success"
+		else
+			redirect "/failure"
+		end
 		#your code here!
 	end
 
